b1c4e0aba6fc09879d1b4a41211f346fe9c28b4c
Nikita Novoselov <sarkomath@outlook.com>
2020-11-23 20:50:45 +1000

    first fix

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        double first_pic = service.calculate(1000000, 12, 9.99);
+        double FirstPic = service.calculate(1000000, 12, 9.99);
         System.out.println("1 year:");
-        System.out.println(first_pic);
+        System.out.println(FirstPic);
 
-        double second_pic = service.calculate(1000000, 24, 9.99);
+        double SecondPic = service.calculate(1000000, 24, 9.99);
         System.out.println("2 years:");
-        System.out.println(second_pic);
+        System.out.println(SecondPic);
 
-        double third_pic = service.calculate(1000000, 36,9.99);
+        double ThirdPic = service.calculate(1000000, 36,9.99);
         System.out.println("3 yeas:");
-        System.out.println(third_pic);
+        System.out.println(ThirdPic);
 
 
     }
